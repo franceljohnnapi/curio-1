f6df81bb7641c1075bebf6275d9c852b6b063ae4
franceljohn <dxd.lux@gmail.com>
2024-05-05 00:55:23 +0800

    admin model

---
+++ models/adminModel.js
@@ -0,0 +1,9 @@
+class admin {
+    constructor(adminname, adminemailadd, adminpassword){
+        this.adminname = adminname;
+        this.adminemailadd = adminemailadd;
+        this.adminpassword = adminpassword;
+   }
+}
+
+module.exports = admin;
