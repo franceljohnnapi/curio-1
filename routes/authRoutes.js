6b42917a4e0363ac93763620dcde3e19bb9c2a6c
franceljohn <dxd.lux@gmail.com>
2024-05-05 00:30:27 +0800

    idk what i did

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,9 +3,15 @@ const router = express.Router();
 
 const { signIn, signUp, adminsignIn, adminsignUp} = require('../controllers/authController');
 
-router.post("http://localhost:3000/", (req, res)) => {
+router.post("http://localhost:3000/", (req, res) => {
     const { fullname, emailadd, password} = req.body;
     const user = { fullname, emailadd, password };
-    signUp(user, (err, ))
-}
+    signUp(user, (err, user) => {
+        if(err){
+            console.error("Error signing up:", err);
+            return res.status(500).json({ error: "Internal Server Error" });
+        }
+        res.status(201).json({ id: user, message: "User signed up successfully" });
+    });
+});
 
